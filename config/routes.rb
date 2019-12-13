7cf5adc27a10b8412980cbc4d32c5a6eaa0ccfc5
khun84 <khun84@gmail.com>
2019-12-13 22:00:07 +0800

    Implemented service request stat endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :dashboard do
-    resources :service_requests, only: :index
+    resources :service_requests, only: :index do
+      collection do
+        get :stat, action: :show, controller: 'service_requests/stat'
+      end
+    end
     resources :service_types, only: :index
     resources :premises, only: :index
   end
