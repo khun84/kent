11841e463045785bd03ef43bcea4628233b88c18
khun84 <khun84@gmail.com>
2019-12-13 23:05:02 +0800

    Add image_url column to premise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_140554) do
+ActiveRecord::Schema.define(version: 2019_12_13_150312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2019_12_13_140554) do
     t.jsonb "notes", default: []
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "service_areas", force: :cascade do |t|
